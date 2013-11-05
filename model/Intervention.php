4c39a60efd7bb3b65ecdf7735ca1ac2558f91e2e
Administrateur <Administrateur@darklite>
2013-11-06 00:21:08 +0100

    Some Model

---
+++ model/Intervention.php
@@ -0,0 +1,14 @@
+<?php
+
+include_once 'model/User.php';
+
+class Intervention {
+
+    private $id;
+    private $User;
+    private $Client;
+    private $text;
+    private $price;
+    private $date;
+
+}
---
+++ model/User.php
@@ -0,0 +1,13 @@
+<?php
+
+include_once 'model/Intervention.php';
+
+Class User {
+
+    private $name;
+    private $surname;
+    private $uid; //uid@42
+    private $id;
+    private $Interventions = array();
+
+}
